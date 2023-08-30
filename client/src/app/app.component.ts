00a6769b911c685c16253a40b08d01dc1195b4f5
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-08-30 14:19:53 +0200

    feat: add getToken() and getUser() methods to auth service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  logged$ = this.authService.logged$;
 
-  constructor() {}
+  constructor(public authService: AuthService) { }
 
 }
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,16 +2,50 @@ import { Injectable } from '@angular/core';
 import axios from 'axios';
 import { Router } from '@angular/router'
 import { CookieService } from 'ngx-cookie-service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private logged = new BehaviorSubject<boolean>(false);
+  private token = this.getToken();
 
+  public logged$ = this.logged.asObservable();
   constructor(
     private router: Router,
-    private cookieService: CookieService  
-  ) { }
+    private cookieService: CookieService,
+  ) {
+    if (this.token !== '') {
+      this.logged.next(true);
+    }
+    console.log('handle auth constructor')
+    console.log('logged: ', this.logged.value);
+    console.log('token: ', this.logged.value);
+  }
+
+  getToken() {
+    return this.cookieService.get('TOKEN');
+  };
+
+  getUser() {
+    axios
+      .get(
+        '/api/get-user',
+        {
+          headers: {
+            'Authorization': `Bearer ${this.getUser()}`
+          }
+        }
+      )
+      .then((result) => {
+        console.log('username: ', result.data.user.username);
+        return result.data.user.username;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
   register(usernameInput: string, passwordInput: string) {
     axios
@@ -24,7 +58,7 @@ export class AuthService {
       .then((result) => {
         // console.log(result);
         alert(result.data.message);
-        this.router.navigate(['']);
+        this.router.navigate(['login']);
       })
       .catch(error => {
         // console.log(error);
