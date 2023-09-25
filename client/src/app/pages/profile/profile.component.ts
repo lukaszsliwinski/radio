d7d4f4673f6d456c7da4d401923e02e8418ecaad
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-09-25 15:49:52 +0200

    feat: style profile navigation

--- client/src/app/pages/profile/profile.component.ts
+++ client/src/app/pages/profile/profile.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
+import {
+  faStar,
+  faClockRotateLeft,
+  faGear
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  public faStar = faStar;
+  public faClockRotateLeft = faClockRotateLeft;
+  public faGear = faGear;
+
   public user$ = this.authService.user$;
 
   constructor(
