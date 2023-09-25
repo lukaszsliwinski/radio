16ebfc9e4687a11aa2b8dbcc5ffbf57e2bad7b35
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-09-25 11:38:57 +0200

    feat: create login page layout

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -2,18 +2,32 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+import {
+  faEye,
+  faEyeSlash
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  public faEye = faEye;
+  public faEyeSlash = faEyeSlash;
+
+  public passwordInputType: 'password' | 'text' = 'password'
+
   public loginForm = new FormGroup({
     usernameInput: new FormControl('', Validators.required),
     passwordInput: new FormControl('', Validators.required)
   });
 
   constructor(private authService: AuthService) {}
+
+  showPassword() {
+    this.passwordInputType === 'password' ? this.passwordInputType = 'text' : this.passwordInputType = 'password';
+  }
 
   submit() {
     const uInput = this.loginForm.value.usernameInput;
