3eb3fc7413682aac2e069a1802cae4fb9b613bec
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-10-03 20:06:05 +0200

    refactor: remove unused code (security reason)

--- server/index.js
+++ server/index.js
@@ -38,15 +38,6 @@ const NODE_ENV = process.env.NODE_ENV;
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../client/dist/client')));
-app.use((request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-  );
-  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
 
 // render client app
 app.get(['/'], (request, response) => {
